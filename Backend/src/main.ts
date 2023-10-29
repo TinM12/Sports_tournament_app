12c0391ac936192e71fd8446a4fd79470ea8ce49
Tin Margetić <tin.margetic@fer.hr>
2023-10-29 14:38:27 +0100

    Slozi CORS

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:[process.env.FRONTEND_URL],
+    origin:['http://localhost:3000', 'https://tournament-app.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
     exposedHeaders: ['x-access-token']
